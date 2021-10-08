97e3ac8b9c0596f6236789c83496b1c6b35f661a
MS2104 <31002@ma-web.nl>
2021-10-08 14:37:51 +0200

    Heb eigenlijk gewoon een mini CMD kopie gemaakt.

--- 02-FilesFolders/huidige_map.py
+++ 02-FilesFolders/huidige_map.py
@@ -1,5 +1,30 @@
 import os
 
-workfolder = os.getcwd()
+def getdir():
+    workfolder = os.getcwd()
+    print("The current directory is:", workfolder)
 
-print("The current directory is:", workfolder)
+def mkdir():
+    foldername = input("Choose a name for the folder: ")
+    
+    dirnamelength = len(foldername)
+    if dirnamelength > 0:
+        os.mkdir(foldername)
+        print(foldername, " has been created.")
+    else:
+        print("Je hebt geen naam ingevoerd.")
+
+while True:
+    print('Type help for a list of commands.')
+    command_line = input(">")
+
+    if command_line == "help":
+        print("getdir - Grabs the directory this program has been executed in.")
+        print("help - Do I really have to explain this one?")
+        print("mkdir - Creates a folder in the directory that this program has been executed in.")
+
+    if command_line == 'getdir':
+        getdir()
+
+    if command_line == 'mkdir':
+        mkdir()
